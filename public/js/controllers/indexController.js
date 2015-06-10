9873996494285fa86030ab7f7eab8e7da719485b
dongdonggun <138856015@qq.com>
2015-06-10 17:53:26 +0800

    change controller

--- public/js/controllers/indexController.js
+++ public/js/controllers/indexController.js
@@ -1,7 +1,13 @@
 
 angular.module('app', [])
     .controller('indexController', function($scope) {
-        $scope.navigation = {
-            name: 'Bootbusiness'
+        $scope.nav = {
+            name: '粉扑扑',
+            video: 'Products and Services',
+            about: '关于扑扑',
+            faq: 'FAQ',
+            contact: '联系我',
+            signUp: '注册',
+            signIn: '登录'
         }
     });
