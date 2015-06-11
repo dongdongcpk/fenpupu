944af000d80526c1bf6a752062f111b9281d6eea
dongdonggun <138856015@qq.com>
2015-06-11 16:53:28 +0800

    change route

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -25,8 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// 路由
+var routes = require('./routes/routes');
+routes(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/js/controllers/indexController.js
+++ public/js/controllers/indexController.js
@@ -2,12 +2,12 @@
 angular.module('app', [])
     .controller('indexController', function($scope) {
         $scope.nav = {
-            name: '粉扑扑',
+            name: '粉扑扑 (｡・`ω´･)',
             video: 'Products and Services',
             about: '关于扑扑',
             faq: 'FAQ',
             contact: '联系我',
             signUp: '注册',
             signIn: '登录'
-        }
+        };
     });
