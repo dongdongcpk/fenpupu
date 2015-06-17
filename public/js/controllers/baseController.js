c8907a4bb8f683d0a6db8ecbd12410f49599012d
dongdonggun <138856015@qq.com>
2015-06-17 22:18:55 +0800

    fix video bug

--- public/js/controllers/baseController.js
+++ public/js/controllers/baseController.js
@@ -2,7 +2,6 @@
 app.controller('baseController', ['$scope', function($scope) {
     $scope.nav = {
         name: '粉扑扑 (｡・`ω´･)',
-        video: 'Products and Services',
         more: '更多扑扑',
         faq: '开发日志',
         contact: '联系我',
--- public/js/controllers/videoDetailController.js
+++ public/js/controllers/videoDetailController.js
@@ -1,10 +1,9 @@
 
-app.controller('videoDetailController', ['$scope', '$routeParams', '$http',
-    function($scope, $routeParams, $http) {
+app.controller('videoDetailController', ['$scope', '$routeParams', '$http', '$sce',
+    function($scope, $routeParams, $http, $sce) {
         var id = $routeParams.id;
         $http.get('getVideo/' + id)
             .success(function(res) {
-                console.log(res);
-                $scope.videoId = res;
+                $scope.embed = $sce.trustAsHtml('<embed height="452" width="544" quality="high" allowfullscreen="true" type="application/x-shockwave-flash" src="http://share.acg.tv/flash.swf" flashvars="'+ res +'" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash"></embed>');
             });
 }]);
